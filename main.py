8de2a214d31aecf434b0a10f99ade06877191cb5
Adam Jesionkiewicz <adam@jesion.pl>
2018-03-10 09:38:16 +0100

    Rewrited program structure

--- main.py
+++ main.py
@@ -5,21 +5,44 @@
 import plotly.plotly
 from plotly.graph_objs import *
 import datetime, math
+import logging
+import unittest
+
+logging.basicConfig(
+    filename="debug.log",
+    level=logging.DEBUG,
+    format="%(asctime)s:%(levelname)s:%(message)s"
+    )
 
 FILENAME = 'testData1.txt'
+NODES_NB = 3
+
+# Filter parameters
+FILTER_R = 0.008
+FILTER_Q = 0.1
 
 
-class DataStream:
+class DataStream(object):
     """
     A few method to operate on raw data set.
     Purpose: convert data from "T=1:N=3:RSSI=-65;17:40:36;" to:
     {'node': 3, 'rssi': -51, 'tag': 1, 'dist': 4.396784253182812, 'ts': datetime.datetime(2018, 2, 20, 17, 40, 29)}
     """
+    cov = float('nan')
+    x = float('nan')
+
     def __init__(self, file_name):
         """
         Constructor with automatic data loader
         """
         self.file_name = file_name
+
+        self.A = 1
+        self.B = 0
+        self.C = 1
+        self.R = FILTER_R
+        self.Q = FILTER_Q
+
         self.data_stream = self.load_data()
 
     def load_data(self):
@@ -27,12 +50,11 @@ class DataStream:
         Open a local disk file (raw data)
         :return: a list of raw records
         """
-        print 'Loading data from file ({})...'.format(self.file_name)
+        logging.debug('Loading data from file ({})...'.format(self.file_name))
         with open(self.file_name, 'r') as data:
             readed_text = data.read()
-        data.close()
         data_stream = readed_text.split(';\n')
-        print '  {} records loaded.'.format(len(data_stream))
+        logging.debug('  {} records loaded.'.format(len(data_stream)))
         return self.parse_data(data_stream)
 
     def parse_data(self, data):
@@ -51,10 +73,22 @@ class DataStream:
                 rssi = int(item.split(':')[2].split(';')[0][5:])
                 temp['rssi'] = rssi
                 temp['dist'] = self.calculate_distance(rssi)
+                # temp['f_dist'] = self.kalman_filter(self.calculate_distance(rssi))
                 temp['ts'] = datetime.datetime(2018, 02, 20, int(item.split(';')[1][:2]),
                                                int(item.split(';')[1][3:5]),
                                                int(item.split(';')[1][6:]))
                 parsed_data.append(temp)
+
+        # Applying filter to specific nodes
+        for node in range(1, NODES_NB + 1):
+            for item in parsed_data:
+                if item['node'] == node:
+                    item['f_dist'] = self.kalman_filter(item['dist'])
+
+            # Reset Kalman variables
+            self.x = float('nan')
+            self.cov = float('nan')
+
         return parsed_data
 
     def get_data(self):
@@ -64,46 +98,7 @@ class DataStream:
         """
         return self.data_stream
 
-    def calculate_distance(self, rssi):
-        """
-        Convert RSSI to real world units (meters)
-        :param rssi:
-        :return: The distance converted from RSSI (in meters)
-        """
-        TX_POWER = -50
-
-        if rssi == 0:
-            return -1
-        ratio = abs(rssi * 1.0 / TX_POWER)
-        if ratio < 1.0:
-            return ratio ** 10
-        else:
-            distance = (3) * (ratio ** 4.595) + 1.111
-            return distance
-
-    def __getitem__(self, item):
-        return self.get_data()[item]
-
-
-class KalmanFilter:
-
-    cov = float('nan')
-    x = float('nan')
-
-    def __init__(self, R, Q):
-        """
-        Constructor
-        :param R: Process Noise
-        :param Q: Measurement Noise
-        """
-        self.A = 1
-        self.B = 0
-        self.C = 1
-
-        self.R = R
-        self.Q = Q
-
-    def filter(self, measurement):
+    def kalman_filter(self, measurement):
         """
         Filters a measurement
         :param measurement: The measurement value to be filtered
@@ -126,46 +121,55 @@ class KalmanFilter:
 
         return self.x
 
+    def calculate_distance(self, rssi):
+        """
+        Convert RSSI to real world units (meters)
+        :param rssi:
+        :return: The distance converted from RSSI (in meters)
+        """
+        TX_POWER = -50
 
-class VisualizeData:
+        if rssi == 0:
+            return -1
+        ratio = abs(rssi * 1.0 / TX_POWER)
+        if ratio < 1.0:
+            return ratio ** 10
+        else:
+            distance = (3) * (ratio ** 4.595) + 1.111
+            return distance
+
+    def __getitem__(self, item):
+        return self.get_data()[item]
+
+
+class VisualizeData():
     """
     Plot data on the diagram (plot.ly)
     """
-    # TODO rewrite whole class
     def __init__(self, data):
         self.data = data
+        logging.debug('VisualizedData - initialize')
 
-    def plot_graph_all_rssi(self):
+    def plot_graph(self, dataset):
         """
-        Tracing raw RSSI data
+        Plot diagram on the server (plotly)
+        :param dataset: choose 'rssi', 'dist', or 'f_dist'
         """
         data = self.data
-        data_x, data_y = [], []
-        for item in data:
-            if item['node'] == 1:
-                data_x.append(item['ts'])
-                data_y.append(item['rssi'])
-        node1 = Scatter(x=data_x, y=data_y)
+        nodes = []
 
-        data_x, data_y = [], []
-        for item in data:
-            if item['node'] == 2:
-                data_x.append(item['ts'])
-                data_y.append(item['rssi'])
-        node2 = Scatter(x=data_x, y=data_y)
+        for node in range(1, NODES_NB + 1):
+            data_x, data_y = [], []
+            for item in data:
+                if item['node'] == node:
+                    data_x.append(item['ts'])
+                    data_y.append(item[dataset])
+            nodes.append(Scatter(x=data_x, y=data_y))
 
-        data_x, data_y = [], []
-        for item in data:
-            if item['node'] == 3:
-                data_x.append(item['ts'])
-                data_y.append(item['rssi'])
-        node3 = Scatter(x=data_x, y=data_y)
+        data = Data(nodes)
+        plotly.plotly.plot(data, filename=dataset+'-line')
 
-        data = Data([node1, node2, node3])
-
-        plotly.plotly.plot(data, filename='rssi-line')
-
-    def plot_graph_distance(self, filtered=1):
+    def plot_graph_distance(self):
         """
         Trasing data converted to real world distances
         :param filtered: 1 Kalman filtered, 2 non-filtered
@@ -176,34 +180,41 @@ class VisualizeData:
         for item in data:
             if item['node'] == 2:
                 data_x.append(item['ts'])
-                data_y.append(item['dist'])
-        node1 = Scatter(x=data_x, y=self.filter_kalman(data_y))
+                data_y.append(item['f_dist'])
+        node1 = Scatter(x=data_x, y=data_y)
 
         data_x, data_y = [], []
         for item in data:
             if item['node'] == 3:
                 data_x.append(item['ts'])
-                data_y.append(item['dist'])
-        node2 = Scatter(x=data_x, y=self.filter_kalman(data_y))
+                data_y.append(item['f_dist'])
+        node2 = Scatter(x=data_x, y=data_y)
 
         data = Data([node1, node2])
 
         plotly.plotly.plot(data, filename='distance-line')
 
-    def filter_kalman(self, data, a=0.008, b=0.1):
-        """
-        Kalman filtering on a set of data
-        :param data: list of raw data
-        :param a: Process Noise
-        :param b: Measurement Noise
-        :return: [LIST] filtered data
-        """
-        kalman = KalmanFilter(a, b)
-        data_filtered = []
-        for i in data:
-            data_filtered.append(kalman.filter(i))
-        return data_filtered
+
+class TestData(unittest.TestCase):
+
+    def test_loader(self):
+        data = DataStream(FILENAME)
+        self.assertGreater(len(data.get_data()), 1)
+
+    def test_parser(self):
+        data = DataStream(FILENAME)
+        self.assertEqual(data[0].keys(), ['node', 'dist', 'f_dist', 'ts', 'tag', 'rssi'])
+
+    def test_filter(self):
+        data = DataStream(FILENAME)
+        test_data = [23, 65]
+        filtered_data = []
+        for i in test_data:
+            filtered_data.append(data.kalman_filter(i))
+        self.assertEqual(filtered_data[1], 44.80769230769231)
 
 
 if __name__ == '__main__':
-    VisualizeData(DataStream(FILENAME)).plot_graph_distance()
+    # unittest.main()
+    VisualizeData(DataStream(FILENAME)).plot_graph('dist')
+    logging.debug('----------------Program ended OK!')
